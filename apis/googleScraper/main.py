2572ba6a896607b8b139bcf6113e211f5c32aa39
matthewnanas <spazeplayz@gmail.com>
2021-06-26 08:32:57 -0400

    init structure

---
+++ apis/googleScraper/main.py
@@ -0,0 +1,58 @@
+import urllib
+import pandas
+import requests
+from newspaper import Article
+from requests_html import HTML
+from requests_html import HTMLSession
+from flask import Flask, redirect, url_for, request
+
+# Send the get request to the specified url
+def getResults(url):
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+    except requests.exceptions.RequestException as error:
+        print(error)
+
+# Parse through response urls
+def scrapeGoogle(query):
+    response = getFullResults(query)
+    return parse_results(response)
+
+# Return the source of the search
+def getFullResults(query):
+    query = urllib.parse.quote_plus(query)
+    response = getResults('https://www.google.com/search?q=%s'%(query))
+    return response
+
+# Structure the full results
+def parse_results(response):
+    output = []
+    results = response.html.find('.tF2Cxc')
+    for result in results:
+        if ("topic" in result.find('.yuRUbf a', first=True).attrs['href']):
+            pass
+        else:
+            article = Article(url=result.find('.yuRUbf a', first=True).attrs['href'], language='en')
+            article.download()
+            article.parse()
+            item = {
+                'title': result.find('h3', first=True).text,
+                'link': result.find('.yuRUbf a', first=True).attrs['href'],
+                'text': result.find('.IsZvec', first=True).text,
+                'image': article.top_image,
+            }
+            output.append(item)
+    return output
+
+app = Flask(__name__)
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = '%s "%s"'%(request.form["query"], request.form["location"])
+    results = scrapeGoogle(query)
+    return str(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
