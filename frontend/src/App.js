d5d8d1344cbf3882589ae0e383945403077b54eb
im-dagger <studyfreak1234@gmail.com>
2021-06-26 23:56:53 +0530

    added header
    dipping for the night

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() 
   return (
     <div className="App">
-      YO IS THIS WORKING
+		<header class="header">
+		<h1 class="logo"><a href="#">VISME</a></h1>
+      <ul class="main-nav">
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">PETITIONS</a></li>
+          <li><a href="#">ROOMS</a></li>
+          <li><a href="#">DETECTOR</a></li>
+          <li><a href="#">HEATMAP</a></li>
+      </ul>
+	</header> 
+  <i class="fas fa-angle-left"></i>
+
+
     </div>
   );
 }
 
 export default App;
+https://kit.fontawesome.com/6564562c61.js
