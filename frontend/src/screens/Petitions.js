77113be41d0c9ccc8d4fd1f52ac087d2d87cecd8
Esketit! <37747855+The-Big-Banana@users.noreply.github.com>
2021-06-27 04:00:17 -0400

    fixed update + added modal

--- frontend/src/screens/Petitions.js
+++ frontend/src/screens/Petitions.js
@@ -1,27 +1,85 @@
-import React, { useState } from 'react';
-import { Button, Jumbotron } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Button, Jumbotron, Modal } from 'react-bootstrap';
 //import Iframe from 'react-iframe'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/News.css';
 
 function Petitions() {
+    // Autofill stuff
+    const [showProfile, setShowProfile] = useState(false);
+    const handleClose = () => setShowProfile(false);
+    const handleShow = () => setShowProfile(true);
+
     const [petitionList, setPetitionList] = useState([{
         "title": "Waiting for petitions...",
         "text": "",
     }]);
-    fetch("http://127.0.0.1:4000/")
-        .then(response => response.json())
-        .then(data => setPetitionList(data));
-    console.log(petitionList)
+    const [petitionIndex, setPetitionIndex] = useState(0)
+    const handleSkip = () => setPetitionIndex(petitionIndex+1);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:4000/")
+            .then(response => response.json())
+            .then(data => setPetitionList(data));
+        console.log(petitionList)
+    });
+
     return (
         <div className="App">
+            <Button variant="primary" onClick={handleShow}>
+                Settings
+            </Button>
+            <Modal show={showProfile} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Fill Settings</Modal.Title>
+                </Modal.Header>
+                <label htmlFor="formGroupExampleInput">First Name</label>
+                <input
+                    type="text"
+                    style={{width: "80%", marginLeft: "10%"}}
+                    id="firstName"
+                />
+                <label htmlFor="formGroupExampleInput">Last Name</label>
+                <input
+                    type="text"
+                    style={{width: "80%", marginLeft: "10%"}}
+                    id="lastName"
+                />
+                <label htmlFor="formGroupExampleInput">Email</label>
+                <input
+                    type="text"
+                    style={{width: "80%", marginLeft: "10%"}}
+                    id="email"
+                    onChange
+                />
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <i class="fas fa-angle-left"></i>
-            <Jumbotron style={{width: "80%", height: "auto", position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
-                <h1>{petitionList[0]["title"]}</h1>
-                <p>{petitionList[0]["text"]}</p>
-            </Jumbotron>
+            <div>
+                <Jumbotron style={{width: "80%", height: "auto", position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
+                    <h1>{petitionList[petitionIndex]["title"]}</h1>
+                    <p>{petitionList[petitionIndex]["text"]}</p>
+                    <div style={{position: 'absolute', left: '50%', transform: 'translate(-50%, 0%)'}}>
+                        <Button variant="danger" onClick={handleSkip}>Skip</Button>
+                        <Button variant="success" style={{marginLeft: 20}} onClick={handleSkip}>Sign</Button>
+                    </div>
+                </Jumbotron>
+            </div>
         </div>
     );
+}
+
+let profile = {
+    "firstName": "Matthew",
+    "lastName": "Nanas",
+    "email": "spazeplayz@gmail.com"
 }
 
 export default Petitions;
